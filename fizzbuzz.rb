748a8740f024173d0f945729125f660520303e21
Jonathan East <jonathan_east@ymail.com>
2020-03-29 02:22:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,2 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num)
-  if num % 15 === 0
-    puts "FizzBuzz"
-  elsif num % 5 === 0
-    puts "Buzz"
-  elsif num % 3 === 0
-    puts "Fizz"
-  else nil
-end
